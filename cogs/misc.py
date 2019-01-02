ac19cb7cefacc6dbd3b75f1bf49783d7307f0702
Ronnie1303 <35928794+Ronnie1303@users.noreply.github.com>
2019-01-02 14:40:59 +0100

    Even better despacito reaction. (#5)
    * Add miscellaneous cog with play despacito reaction
    
    * Add miscellaneous cog with play despacito reaction
    
    * Fix on bee despacito response bug
    
    * Add reaction to despacito message response
    
    * Removed merge head from misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 from discord.ext import commands
 
+letters_emoji = ["🇩", "🇪", "🇸", "🇵", "🇦", "🇨", "🇮", "🇹", "🇴"]
 
 class Misc:
     def __init__(self, bot):
@@ -13,6 +14,8 @@ class Misc:
 
         if "bee play despacito" in message.content.lower() or "bee, play despacito" in message.content.lower():
             channel = message.channel
+            for letter in letters_emoji:
+                await message.add_reaction(letter)
             await channel.send("https://www.youtube.com/watch?v=kJQP7kiw5Fk")
 
 
